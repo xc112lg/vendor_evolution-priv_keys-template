bf6f2bbc98a35b3bb7d2f1f0ec3052e5193c5868
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-09-14 06:06:21 +0800

    Update make_key.sh

--- make_key.sh
+++ make_key.sh
@@ -6,4 +6,4 @@
 set -u
 bash <(sed "s/2048/${2:-2048}/;/Enter password/,+1d" ../../../development/tools/make_key) \
     $1 \
-    '/C=US/ST=California/L=Mountain View/O=Android/OU=Android/CN=Android/emailAddress=android@android.com'
+    '/C=PH/ST=Manila/L=Manila/O=Android/OU=Android/CN=Android/emailAddress=rexc1835@gmail.com'
